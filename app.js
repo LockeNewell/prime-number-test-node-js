4e7ed513e4f749a86a8279a5d1b05625fb01b096
Locke Newell <locke.newell@gmail.com>
2021-10-12 13:13:01 -0500

    fix to work as server

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const { mainModule } = require("process");
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
 
 let tasks = ['create a function that takes in a number',
         'pass back the lowest root greater then 1 if not prime',
@@ -8,10 +12,19 @@ let hint = 'make use of the % operator ';
 
 let stretchTask = 'create unit tests the function';
 
-function main() {
-    item = 501;
-    console.log(`lowest root of ${item} is ${numberCheck(item)}`);
-}
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    item = 22;
+    res.end(`lowest root of ${item} is ${numberCheck(item)}`);
+  });
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
+
 
 function numberCheck(input) {
     for(let i = 2; i<input; i++) {
